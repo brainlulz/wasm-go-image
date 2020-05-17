78e2962e62a09043c96953a49807b93a1b03829e
Alex Labonne <>
2020-05-17 22:37:47 +0200

    feat: add identicon generator

--- main.go
+++ main.go
@@ -1,38 +1,64 @@
 package main
 
 import (
-	"syscall/js"
 	"bytes"
-	"image"
-	_ "image/png"
-	_ "image/jpeg"
 	"fmt"
+	"image"
+	"image/jpeg"
+	_ "image/jpeg"
+	_ "image/png"
+	"syscall/js"
+
+	"github.com/aofei/cameron"
 )
 
 func readImage(this js.Value, args []js.Value) interface{} {
 	array := args[0]
-	buff  := make([]uint8, array.Get("byteLength").Int())
+	buff := make([]uint8, array.Get("byteLength").Int())
 	js.CopyBytesToGo(buff, array)
 
-
 	reader := bytes.NewReader(buff)
-	_, _, err := image.Decode(reader)
-
+	sourceImg, _, err := image.Decode(reader)
 
 	if err != nil {
 		println("An error occured while reading the image")
 		fmt.Println(err)
 	}
 
+	fmt.Println(sourceImg)
 	println("Image loaded")
 	return nil
 	// Now the sourceImg is an image.Image with which we are free to do anything!
+}
+
+func identicon(this js.Value, i []js.Value) interface{} {
+	array := i[0].String()
+
+	imgIcon := cameron.Identicon(
+		[]byte(array),
+		540,
+		60,
+	)
+	buf := bytes.Buffer{}
+	jpeg.Encode(
+		&buf,
+		imgIcon,
+		&jpeg.Options{
+			Quality: 100,
+		},
+	)
+
+	dst := js.Global().Get("Uint8Array").New(len(buf.Bytes()))
+	js.CopyBytesToJS(dst, buf.Bytes())
+
+	return dst
 }
 
 func registerCallbacks() {
 	emptyWASMObject := make(map[string]interface{})
 	js.Global().Set("WASMGo", js.ValueOf(emptyWASMObject))
 	js.Global().Get("WASMGo").Set("loadImage", js.FuncOf(readImage))
+	js.Global().Get("WASMGo").Set("identicon", js.FuncOf(identicon))
 }
 
 func main() {
